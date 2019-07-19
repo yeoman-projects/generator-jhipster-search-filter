8a25a9e9e2bb42ef36c681f482acfd5046d1fdb8
Enrico Costanzi <enrico.costanzi@gmail.com>
2019-07-19 17:44:34 +0200

    stuff

--- generators/entity-client/index.js
+++ generators/entity-client/index.js
@@ -1,28 +1,18 @@
 /* eslint-disable consistent-return */
-const chalk = require('chalk');
 const EntityClientGenerator = require('generator-jhipster/generators/entity-client');
 const writeFiles = require('./files').writeFiles;
 
-
 module.exports = class extends EntityClientGenerator {
     constructor(args, opts) {
-        super(args, { fromBlueprint: true, ...opts }); // fromBlueprint variable is important
-
-        if (!this.jhipsterContext) {
-            this.error('This is a JHipster blueprint and should be used only like jhipster --blueprint myblueprint');
-        }
+        super(args, Object.assign({ fromBlueprint: true }, opts)); // fromBlueprint variable is important
     }
 
     get writing() {
-
-        console.log("su_ka")
-        const phaseFromJHipster = super._writing();
-        const customPhaseSteps = {
+        return {
             writeAdditionalFile() {
                 writeFiles.call(this);
             }
         };
-        return Object.assign(phaseFromJHipster, customPhaseSteps);
     }
 
     get end() {
